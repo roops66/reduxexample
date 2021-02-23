aa6a6776f53055578868a55f8970e9d3c14538fc
Mandeep Rooprai <mandeep.rooprai@kcl.ac.uk>
2021-02-23 21:32:38 +0000

    Completed tutorial, but refactoring to do (lifecyle method)

---
+++ src/components/Posts.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions';
+
+class Posts extends Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.newPost) {
+            this.props.posts.unshift(prevProps.newPost);
+            console.log(prevProps.newPost);
+        }
+    }
+
+    render() {
+        const postItems = this.props.posts.map(post => (
+            <div key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+            </div>
+        ));
+        return (
+            <div>
+                <h1>Posts</h1>
+                {postItems}
+            </div>
+        );
+    }
+}
+
+Posts.propTypes = {
+    fetchPosts: PropTypes.func.isRequired,
+    posts: PropTypes.array.isRequired,
+    newPost: PropTypes.object
+}
+
+const mapStateToProps = state => ({
+    posts: state.posts.items,
+    newPost: state.posts.item       // The newPost prop comes from the reducer state.posts and .item is the state we want to put into the newPost prop. 
+})
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
